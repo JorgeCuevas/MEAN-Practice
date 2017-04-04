5cc7d5b3bc965a434da3cb5174084d2a50c67f56
Jorge Cuevas Quezada <jcuevas@segurospein.com>
2017-04-04 16:11:37 -0400

    working with flash message

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {RouterModule, Routes} from '@angular/router';
-
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -40,7 +40,9 @@ const appRoutes :Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
+    FlashMessagesModule,
     RouterModule.forRoot(appRoutes),
+
   ],
   providers: [ValidateService],
   bootstrap: [AppComponent]
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate/validate.service' ;
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
 	selector: 'app-register',
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 	username: String;
 	password: String;
 
-	constructor(private validateService: ValidateService) { }
+	constructor(private validateService: ValidateService, private flashMessages: FlashMessagesService) { }
 
 	ngOnInit() {
 	}
@@ -27,12 +28,12 @@ export class RegisterComponent implements OnInit {
 
 
 		if(!this.validateService.validateRegister(user)){
-			console.log('Invalid user');
+			 this.flashMessages.show('Invalid User!, ', { cssClass: 'alert-danger', timeout: 8000 });
 			return false;
 		}
 
 		if(!this.validateService.validateEmail(user.email)){
-			console.log('Invalid email');
+			this.flashMessages.show('Invalid Email!, ', { cssClass: 'alert-danger', timeout: 8000 });
 			return false;
 		}
 	}
